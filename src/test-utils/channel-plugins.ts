5011a0a51bcb48d398a2d0b6a974c87f784e5324
clawd-a01 <clawd.a01@gmail.com>
2026-06-12 01:26:36 +0000

    fix(build): add missing tool schema contribution registry defaults

--- src/test-utils/channel-plugins.ts
+++ src/test-utils/channel-plugins.ts
@@ -16,6 +16,7 @@ export type TestChannelRegistration = {
 export const createTestRegistry = (channels: TestChannelRegistration[] = []): PluginRegistry => ({
   plugins: [],
   tools: [],
+  toolSchemaContributions: [],
   hooks: [],
   typedHooks: [],
   channels: channels as unknown as PluginRegistry["channels"],
